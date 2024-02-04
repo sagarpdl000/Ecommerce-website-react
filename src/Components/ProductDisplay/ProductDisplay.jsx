b9129c2cde8e52f8111b83b984944f81ea187ede
Sagar poudel <pdlsgr62@gmail.com>
2024-02-04 09:14:23 +0545

    Cart logic

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
-
 import star from '../Assets/star.png'
+import { useContext } from 'react';
+import { ShopContext } from '../../Context/ShopContext';
 
 
 
 const ProductDisplay = (props) => {
     const {product} = props;
+    const {addToCart} = useContext(ShopContext);
   return (
-    <div className='mx-16 font-poppins grid grid-cols-2 my-4'>
-        <div className='flex' id='left '>
+    <div className='mx-16 font-poppins grid grid-cols-2 my-10 '>
+        <div className='flex  ' id='left '>
             <div className=''>
                 <img src={product.image} alt="" className='h-40 w-40 object-cover'/>
                 <img src={product.image} alt=""className='h-40 w-40 object-cover pt-2' />
                 <img src={product.image} alt=""className='h-40 w-40 object-cover pt-2' />
-                <img src={product.image} alt="" className='h-40 w-40 object-cover pt-2'/>
+                
             </div>
 
     
-            <div className='pl-2'>
-                <img src={product.image} alt="" className=' w-full object-cover h-97' />
+            <div className='pl-2 '>
+                <img src={product.image} alt="" className=' ' />
 
             </div>
         </div>
@@ -48,14 +50,17 @@ const ProductDisplay = (props) => {
             <div>
                 <h1>Select Size</h1>
                 <div className='flex my-2 gap-4'>
-                    <div className=' px-3 bg-gray-400'>S</div>
-                    <div className=' px-3 bg-gray-400'>M</div>
-                    <div className=' px-3 bg-gray-400'>L</div>
-                    <div className=' px-3 bg-gray-400'>XL</div>
-                    <div className=' px-3 bg-gray-400'>XXL</div>
+                    <div className=' px-3 bg-gray-400'><button>S</button></div>
+                    <div className=' px-3 bg-gray-400'><button>M</button></div>
+                    <div className=' px-3 bg-gray-400'><button>L</button></div>
+                    <div className=' px-3 bg-gray-400'><button>XL</button></div>
+                    <div className=' px-3 bg-gray-400'><button>XXL</button></div>
+
                 </div>
             </div>
-            <button className='my-2 bg-red-500 px-6 py-2 uppercase text-white duration-300 hover:scale-105'>Add to cart</button>
+
+            <button onClick={()=>{addToCart(product.id)}} className='my-2 bg-red-500 px-6 py-2 uppercase text-white duration-300 hover:scale-105'>Add to cart</button>
+
             <p><span>Category: </span>Man, Winter Jacket</p>
             
 
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import All_Product from "../Components/Assets/All_Product";
 
 
@@ -6,9 +6,34 @@ import All_Product from "../Components/Assets/All_Product";
 
 
 export const ShopContext = createContext(null);
+const getDefaultCart = () =>{
+    let cart = {};
+    for (let index = 0; index < All_Product.length + 1; index++) {
+        
+        cart[index] = 0;
+        
+    }
+    return cart;
+}
 
 const ShopContextProvider = (props) =>{
-        const contextValue = {All_Product}
+
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+        
+
+        const addToCart = (itemId) =>{
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+
+        const removeFromCart = (itemId) =>{
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+            console.log(cartItems);
+        }
+        const contextValue = {All_Product, cartItems, addToCart,removeFromCart};
+
+       
+                                
+
 
         return(
             <ShopContext.Provider value={contextValue}>
